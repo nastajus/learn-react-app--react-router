ad93a1e12b546e9537d1371817319b391a5aa14c
Ian Nastajus <ian.nastajus@gmail.com>
2018-02-28 22:32:11 -0500

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -10,23 +10,12 @@ class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        {/* problem #1: no default path, loading url root "/" doesn't show Header */}
         <Route path={"/"} component={Root} >
-
-          {/* solution to p#1 */}
           <IndexRoute component={Home} />
-
-          {/* works: loading url path "/user" or "/home" shows Header AND child component */}
           <Route path={"user"} component={User}/>
           <Route path={"home"} component={Home}/>
         </Route>
-
-        {/* ordering does matter, will not arrive here, since already captured above */}
-        <Route path={"home"} component={Home}/>
-
-        {/* alternatively the following WON'T use the Root component */}
         <Route path={"home-single"} component={Home}/>
-
       </Router>
     );
   }
